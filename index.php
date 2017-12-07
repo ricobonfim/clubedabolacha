d6d1e7b1ac2a2529dc7166c57f2c46bc0b301b65
Ricardo Bonfim <ricobonfim@gmail.com>
2017-12-07 08:46:01 -0200

    Adiciona gif animada

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
     <title>Controle da caixinha</title>
     <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <style type="text/css">
+        #bob-gif {
+            width: 300px;
+        }
+    </style>
 </head>
 <body>
     <br/>
@@ -77,24 +83,36 @@
         <br>
 
         <h4>Últimos gastos</h4>
-        <table class='table table-hover'>
-            <thead>
-                <tr>
-                    <th>Data</th>
-                    <th>Gasto</th>
-                    <th>Comprovante</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach (array_reverse($ultimosGastos) as $gasto) : ?>
-                    <tr>
-                        <td><?php echo $gasto['data']; ?></td>
-                        <td><?php echo "R$" . number_format($gasto['valor'], 2, ',', '.'); ?></td>
-                        <td><a href="comprovantes/<?php echo str_replace('/', '_', $gasto['data']) ?>.jpg">Comprovante</a></td>
-                    </tr>            
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-</div>
+        <div class="row">
+            <div class="col-md-9">
+                <table class='table table-hover'>
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Gasto</th>
+                            <th>Comprovante</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach (array_reverse($ultimosGastos) as $gasto) : ?>
+                            <tr>
+                                <td><?php echo $gasto['data']; ?></td>
+                                <td><?php echo "R$" . number_format($gasto['valor'], 2, ',', '.'); ?></td>
+                                <td><a href="comprovantes/<?php echo str_replace('/', '_', $gasto['data']) ?>.jpg">Comprovante</a></td>
+                            </tr>            
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>                
+            </div>
+            <div class="col-md-3">
+                <video autoplay loop id="bob-gif">
+                  <source src="bob-esponja.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+
+
+
+    </div>
 </body>
 </html>
