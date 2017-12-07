ee4cee3f4fc4a33b2981f0827398ebb7572549ac
Ricardo Bonfim <ricobonfim@gmail.com>
2017-12-07 08:30:46 -0200

    Adiciona custos e contribuicoes

--- index.php
+++ index.php
@@ -15,24 +15,57 @@
             'valor' => 14.92
         );
 
+        $ultimosGastos[] = array(
+            'data' => "06/12/2017",
+            'valor' => 21.43
+        );
+
         $saldos[] = array(
             'dataFechamento' => "01/12/2017",
-            'caixaAtual' => 65.00
+            'totalContribuicoes' => 65,
+            'gastosUltimoFechamento' => 0
         );
+
+        $saldos[] = array(
+            'dataFechamento' => "07/12/2017",
+            'totalContribuicoes' => 35,
+            'gastosUltimoFechamento' => 36.35
+        );
+
+        function getSaldoAtual(array $saldos) : float {
+            $total = 0;
+            foreach ($saldos as $saldo) {
+                $total += $saldo['totalContribuicoes']-$saldo['gastosUltimoFechamento'];
+            }
+            return $total;
+        }
+
+        function getSaldoParcial(array $saldos, int $index) : float {
+            $saldosParcial = array_slice($saldos, 0, $index+1);
+            $total = 0;
+            foreach ($saldosParcial as $saldo) {
+                $total += $saldo['totalContribuicoes']-$saldo['gastosUltimoFechamento'];
+            }
+            return $total;
+        }
     ?>
-        <h4>Saldo caxinha: <?php echo "R$" . number_format($saldos[count($saldos)-1]['caixaAtual'], 2, ',', '.'); ?></h4>
+        <h4>Saldo caxinha: <?php echo "R$" . number_format(getSaldoAtual($saldos), 2, ',', '.'); ?></h4>
         <table class='table table-hover'>
             <thead>
                 <tr>
                     <th>Data Fechamento</th>
-                    <th>Total</th>
+                    <th>Custos ultimo fechamento</th>
+                    <th>Total Contribuições</th>
+                    <th>Saldo</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach (array_reverse($saldos) as $saldo) : ?>
+                <?php foreach (array_reverse($saldos) as $index => $saldo) : ?>
                     <tr>
                         <td><?php echo $saldo['dataFechamento']; ?></td>
-                        <td><?php echo "R$" . number_format($saldo['caixaAtual'], 2, ',', '.'); ?></td>
+                        <td><?php echo "R$" . number_format($saldo['gastosUltimoFechamento'], 2, ',', '.'); ?></td>
+                        <td><?php echo "R$" . number_format($saldo['totalContribuicoes'], 2, ',', '.'); ?></td>
+                        <td><?php echo "R$" . number_format(getSaldoParcial($saldos, $index), 2, ',', '.'); ?></td>
                     </tr>            
                 <?php endforeach; ?>
             </tbody>
